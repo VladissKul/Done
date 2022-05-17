49789da2bbaf820e11055823fd710b2ea7e4719b
VladissKul <vladislav.kulakov.2002@mail.ru>
2022-05-17 18:16:37 +0300

    add requrements

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -122,3 +122,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = '/login'
+
+try:
+    from .local_settings import *
+except ImportError:
+    print('Looks like no local file. You must be on production')
